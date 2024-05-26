f56f98656e436c6a0163219b2948ef6d8276c15d
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-05-26 12:25:00 +0600

    Implemented react hook form for patient register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,52 @@
+import {Box, Button, Container, Grid, Stack, TextField, Typography} from "@mui/material";
+import Image from "next/image";
+import assets from '@/assets'
+import Link from "next/link";
 
 
 const LoginPage = () => {
   return (
-    <div>
-      login page
-    </div>
+    <Container>
+      <Stack sx={{justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <Box sx={{
+          maxWidth: 600,
+          width: '100%',
+          boxShadow: 1,
+          borderRadius: 1,
+          p: 4
+        }}>
+          <Stack sx={{justifyContent: 'center', alignItems: 'center'}}>
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="Logo" />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={900}>
+                Login PH HealthCare
+              </Typography>
+            </Box>
+          </Stack>
+          <Box sx={{textAlign: 'center'}}>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={6}>
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Email" type="email" variant="outlined" />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Password" type="password" variant="outlined" />
+                </Grid>
+              </Grid>
+              <Typography sx={{marginTop: '10px'}} textAlign="end" component="p" fontWeight={300}>
+                  Forgot Password? <Link href="/register">Click Here</Link>
+              </Typography>
+              <Button sx={{margin: '10px 0'}} fullWidth={true}>Login</Button>
+              <Typography component="p" fontWeight={300}>
+                  Don&apos;t have an account? <Link href="/register">Create an account</Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,33 @@
+'use client'
 import {Box, Button, Container, Grid, Stack, TextField, Typography} from "@mui/material";
 import Image from "next/image";
 import assets from '@/assets'
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form"
+
+interface IPatientData {
+  name: string
+  email: string
+  contactNumber: string
+  address: string
+}
+
+interface IPatientRegisterFormData {
+  password: string;
+  patient: IPatientData;
+}
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IPatientRegisterFormData>()
+  const onSubmit: SubmitHandler<IPatientRegisterFormData> = (data) => console.log(data)
   return (
-    <Container sx={{padding: '50px'}}>
-      <Stack sx={{justifyContent: 'center', alignItems: 'center'}}>
+    <Container>
+      <Stack sx={{justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
         <Box sx={{
           maxWidth: 600,
           width: '100%',
@@ -25,25 +46,25 @@ const RegisterPage = () => {
             </Box>
           </Stack>
           <Box sx={{textAlign: 'center'}}>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <Grid container spacing={3} my={1}>
                 <Grid item md={12}>
-                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Name" variant="outlined" />
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Name" variant="outlined" {...register("patient.name")} />
                 </Grid>
                 <Grid item md={6}>
-                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Email" type="email" variant="outlined" />
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Email" type="email" variant="outlined" {...register("patient.email")} />
                 </Grid>
                 <Grid item md={6}>
-                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Password" type="password" variant="outlined" />
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Password" type="password" variant="outlined" {...register("password")} />
                 </Grid>
                 <Grid item md={6}>
-                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Contact Number" type="tel" variant="outlined" />
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Contact Number" type="tel" variant="outlined" {...register("patient.contactNumber")} />
                 </Grid>
                 <Grid item md={6}>
-                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Address" variant="outlined" />
+                  <TextField size="small" fullWidth={true} id="outlined-basic" label="Address" variant="outlined" {...register("patient.address")} />
                 </Grid>
               </Grid>
-              <Button sx={{margin: '20px 0'}} fullWidth={true}>Register</Button>
+              <Button type="submit" sx={{margin: '20px 0'}} fullWidth={true}>Register</Button>
               <Typography component="p" fontWeight={300}>
                   Do you already have an account? <Link href="/login">Login</Link>
               </Typography>
